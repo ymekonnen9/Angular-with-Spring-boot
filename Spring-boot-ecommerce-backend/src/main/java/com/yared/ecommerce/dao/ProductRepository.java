e85fa947c3e16fdeab450f08b361a9a8a447cd9e
Yared Mekonnen <yaredmekonnen999@gmail.com>
2024-08-11 18:34:15 -0500

    Entities and Daos
    Created Product and Product Category entities with their DAOs. I used Spring Data Rest for exposing CRUD endpoints.

---
+++ Spring-boot-ecommerce-backend/src/main/java/com/yared/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.yared.ecommerce.dao;
+
+import com.yared.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
